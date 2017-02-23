7d41aeb0998debdc338883564a4f907586d60bf1
Jeb Bearer <jbearer@hmc.edu>
2017-02-23 14:54:29 -0800

    Create add_item, add_items interface in bin model.
    These helper functions take advantage of the fact that our schema
    is set up to avoid item duplication. If there is an existing item
    with the same properties, then the bin simply takes a reference to
    that item. Otherwise, a new item is created and added to the bin.

--- recycling-helper/app/models/bin.rb
+++ recycling-helper/app/models/bin.rb
@@ -1,4 +1,26 @@
 class Bin < ApplicationRecord
   belongs_to :city
   has_and_belongs_to_many :items
+
+  # Add an item to this bin, reusing an existing item if possible. Returns the added item.
+  def add_item!(name, category)
+    existing = Item.where({name: name, category: category})
+    if existing.empty?
+      result = items.create(name: name, category: category)
+    else
+      items << existing[0]
+      result = existing[0]
+    end
+
+    result
+  end
+
+  # Add a series of items to this bin, reusing existing items where possible. Items must respond to
+  # each, and each item should be a hash containing keys :name and :category.
+  def add_items!(items)
+    items.map do |item|
+      add_item! item[:name], item[:category]
+    end
+  end
+
 end
--- recycling-helper/test/models/bin_test.rb
+++ recycling-helper/test/models/bin_test.rb
@@ -9,4 +9,28 @@ class BinTest < ActiveSupport::TestCase
     assert_equal Item.find(items(:newspaper).id, items(:plastic_bottle).id),
                  bins(:claremont_recycling).items
   end
+
+  test "can add existing item" do
+    bin = bins(:claremont_recycling)
+    item = Item.create(name: 'new item', category: categories(:paper))
+    item_in_bin = bin.add_item! 'new item', categories(:paper)
+
+    # Should just refer to the same item
+    assert_includes bin.items, item, 'new item is not in bin'
+    assert_equal item, bin.items.find(item.id), 'new item in bin does not refer to existing item'
+    assert_equal item, item_in_bin, 'new item in bin was not returned from add_item'
+  end
+
+  test "can create new item" do
+    bin = bins(:claremont_recycling)
+    items = Item.select('id')
+    item = bin.add_item! 'new item', categories(:paper)
+
+    assert_not_includes items, item.id, 'a new item was not created'
+    assert_equal item, Item.find(item.id), 'the new item was not in the database'
+    assert_equal item, bin.items.find(item.id), 'the new item was not in the bin\'s items'
+
+    assert_equal 'new item', item.name
+    assert_equal categories(:paper), item.category
+  end
 end
