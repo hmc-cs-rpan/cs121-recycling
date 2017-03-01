5404e57d43c4b7eb9c457a1c3bd58e607b8db7c0
Faiz Abdulali <fabdulal@students.pitzer.edu>
2017-03-01 14:47:12 -0800

    FrontEnd initial commit

--- recycling-helper/config/routes.rb
+++ recycling-helper/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :terms
+  resources :articles
+  resources :categories
+  resources :items
+  resources :bins
   devise_for :admins
   devise_for :city_officials
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cities
+  root 'categories#index'
 end
