5fc8732814df701ca250bdab8c1b339af1ccaaf1
ssamar2018 <shailee.samar@gmail.com>
2017-03-16 15:32:55 -0700

    Gave each city a unique set of categories

--- recycling-helper/app/models/city.rb
+++ recycling-helper/app/models/city.rb
@@ -8,7 +8,8 @@ class City < ApplicationRecord
 
   has_many :bins, inverse_of: :city
   has_many :items, through: :bins
-  has_many :categories, through: :items
+  #has_many :categories, through: :items
+  has_many :categories, -> { distinct }, through: :items
   has_many :officials, class_name: 'CityOfficial', inverse_of: :city
 
   # Add a bin to the city and return the new bin. If the bin is not unique (this city already has
