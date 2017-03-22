99c36e1d9fc66c6a042eb759544e27339a01f6a5
Jeb Bearer <jbearer@hmc.edu>
2017-03-21 22:52:15 -0700

    Convert JS to CoffeeScript

--- recycling-helper/config/initializers/assets.rb
+++ recycling-helper/config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( application.coffee )
 Rails.application.config.assets.precompile += %w( cities.css )
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
-Rails.application.config.assets.precompile += %w( cities.js )
+Rails.application.config.assets.precompile += %w( cities.coffee )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
