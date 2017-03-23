1cc30b4dd4806b2b0cd8b6ab6987cd5e84958340
Jeb Bearer <jbearer@hmc.edu>
2017-03-22 21:10:46 -0700

    Create flash message utilities.
    * Flash messages are displayed at the top of the page content
      section of the application layout.
    * CoffeeScript helpers are provided for dyamically generating flash
      messages on the client side

--- recycling-helper/config/initializers/assets.rb
+++ recycling-helper/config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( application.coffee )
 Rails.application.config.assets.precompile += %w( cities.css )
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
 Rails.application.config.assets.precompile += %w( cities.js )
