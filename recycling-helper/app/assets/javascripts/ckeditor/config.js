2ba33547526bc557df95293953b07fe436df66cc
Jeb Bearer <jbearer@hmc.edu>
2017-04-12 11:28:59 -0700

    Simplify CKEditor toolbar

--- recycling-helper/app/assets/javascripts/ckeditor/config.js
+++ recycling-helper/app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,50 @@
 CKEDITOR.editorConfig = function(config) {
-    config.extraPlugins = 'autogrow';
-    config.autoGrow_onStartup = true;
+  /* Filebrowser routes */
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of a script that handles file uploads in the Flash dialog.
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+
+  // The location of a script that handles file uploads in the Image dialog.
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+
+  // The location of a script that handles file uploads.
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+  config.allowedContent = true;
+
+  // Toolbar groups configuration.
+  config.toolbar = [
+    { name: 'clipboard', groups: [ 'undo', 'clipboard' ], items: [ 'Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste' ] },
+    { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+    // { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+    // { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+    { name: 'links', items: [ 'Link' ] },
+    { name: 'insert', items: [ 'Image' ] },
+    { name: 'paragraph', groups: [ 'list', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] }
+  ];
+
+  config.toolbar_mini = [
+    { name: 'paragraph', groups: [ 'list', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+    { name: 'styles', items: [ 'Font', 'FontSize' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline' ] },
+    { name: 'insert', items: [ 'Image' ] }
+  ];
+
+  // Autosize to fit content
+  config.extraPlugins = 'autogrow';
+  config.autoGrow_onStartup = true;
 };
